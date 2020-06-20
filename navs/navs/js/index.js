553b37f5dd07cd2d9c6a26aae7fc4a01983214c7
Jay Franks <59868381+JayFranks99@users.noreply.github.com>
2020-06-20 19:19:18 +0100

    Dropdown nav added to navs collection

---
+++ navs/navs/js/index.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "100%"; //expand from 0% to 100%
+  document.getElementById("myNav").style.overflow = "hidden"; //prevents scorlling
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%"; //reduce from 100% to 0%
+}
